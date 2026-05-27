af93c46c6048569d4e59d9acff2c58298860f7af
Ramiro Acevedo (Rol: Paco) <ramiroacev@gmail.com>
2026-05-27 14:39:45 +0000

    TP2OE-33 Se crearon las funciones para generar grafico y un reporte .csv y se corrigieron las rutas relativas

--- scripts/analisis_ventas.py
+++ scripts/analisis_ventas.py
@@ -1,5 +1,8 @@
+from pathlib import Path
 import csv
 import os
+import matplotlib.pyplot as plt
+import pandas as pd
 
 # Programa de análisis de ventas usando el dataset de Kaggle:
 # https://www.kaggle.com/datasets/mohammadtalib786/retail-sales-dataset
@@ -10,15 +13,20 @@ import os
 # 4. Calcular ventas por mes y detectar el mes con más ventas
 # 5. Guardar los datos en un archivo .txt
 
-ARCHIVO_CSV = "../datos/retail_sales_dataset.csv"
-CARPETA_SALIDA = "../resultados"
-ARCHIVO_SALIDA = os.path.join(CARPETA_SALIDA, "resumen_ventas.txt")
+#AL utilizar pathlib evitamos problemas por lo que funciona en 
+# windows y en linux por lo que hace el proyecto portable
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Rutas
+ARCHIVO_CSV = BASE_DIR / "datos" / "retail_sales_dataset.csv"
+CARPETA_SALIDA = BASE_DIR / "resultados"
+ARCHIVO_SALIDA = CARPETA_SALIDA / "resumen_ventas.txt"
 
 def lectura():
     try:
         """Lee el archivo CSV y devuelve un diccionario."""
         with open(ARCHIVO_CSV, "r", encoding="utf-8-sig") as archivo:
-            """Guardamos los datos en una list(), ya que al llegar al return la funcion, el archivo se cierra"""
+            #Guardamos los datos en una list(), ya que al llegar al return la funcion, el archivo se cierra
             return list(csv.DictReader(archivo))
 
     except FileNotFoundError:
@@ -31,25 +39,36 @@ def lectura():
 
 def ventas_totales_por_categoria():
     """Calcula el total de ventas de una categoría ingresada por el usuario."""
-    categoria = input("\nIngrese una categoría (Beauty, Clothing o Electronics): ").capitalize()
-    venta_total = 0
-   
-    """LLamamos a la funcion lectura() y guardamos los datos en una variable"""
-    lector = lectura()
-
-    for fila in lector:
-        if fila["Product Category"] == categoria:
-            cantidad = int(fila["Quantity"])
-            precio = float(fila["Price per Unit"])
-            subtotal = cantidad * precio
-            venta_total += subtotal
+    try:
+        categoria = input("\nIngrese una categoría (Beauty, Clothing o Electronics): ").capitalize()
+        venta_total = 0
     
-    print(f"\nLas ventas totales de {categoria} fueron: ${venta_total:,.2f}")
+        #LLamamos a la funcion lectura() y guardamos los datos en una variable
+        lector = lectura()
+
+        for fila in lector:
+            if fila["Product Category"] == categoria:
+                cantidad = int(fila["Quantity"])
+                precio = float(fila["Price per Unit"])
+                subtotal = cantidad * precio
+                venta_total += subtotal
+        
+        print(f"\nLas ventas totales de {categoria} fueron: ${venta_total:,.2f}")
+    
+    except KeyError as e:
+        print(f"Error: No existe la columna {e} en el archivo CSV.")
+
+    except ValueError as e:
+        print(f"Error: {e}")
+
+    except Exception as e:
+        print(f"Ocurrió un error inesperado: {e}")
 
 def producto_mas_vendido():
     """Encuentra la categoría de producto con mayor cantidad de ventas."""
     ventas_producto = {}
     
+    #Abrimos el archivo para leer los datos
     lector = lectura()
 
     for fila in lector:
@@ -64,6 +83,7 @@ def ventas_por_mes():
     """Calcula las ventas totales por mes y muestra el mes con más ventas."""
     venta_mes = {}
    
+    #Abrimos el archivo para leer los datos
     lector = lectura()
 
     for fila in lector:
@@ -98,43 +118,53 @@ def imprimir_ventas_por_mes(venta_mes):
 
 
 def buscador():
-    """Cuenta cuántas coincidencias hay de una categoría iventas_por_mesngresada por el usuario."""
-    buscar = input("\nIngrese una categoría a buscar (Beauty, Clothing o Electronics): ").capitalize()
-    contador = 0
-    
-    lector = lectura()
-
-    for fila in lector:
-        if fila["Product Category"] == buscar:
-            contador += 1
-    
-    print("\nRESULTADOS:")
-    print(f"Se encontraron {contador} coincidencias de la categoría {buscar}.")
-
-
-def generar_archivo():
-    """Genera un archivo .txt con todos los datos"""
+    """Cuenta cuántas coincidencias hay de una categoría ingresada por el usuario."""
     try:
-        """inicializamos la variable vacia"""
-        ventas_total = 0
-
-        """Abrimos el archivo para leer los datos"""
+        buscar = input("\nIngrese una categoría a buscar (Beauty, Clothing o Electronics): ").capitalize()
+        contador = 0
+        
+        #Abrimos el archivo para leer los datos
         lector = lectura()
 
         for fila in lector:
-            """Como usamos DictReader, accedemos al nombre de la columna directamente"""
+            if fila["Product Category"] == buscar:
+                contador += 1
+        
+        print("\nRESULTADOS:")
+        print(f"Se encontraron {contador} coincidencias de la categoría {buscar}.")
+
+    except KeyError as e:
+        print(f"Error: No existe la columna {e} en el archivo CSV.")
+
+    except ValueError as e:
+        print(f"Error: {e}")
+
+    except Exception as e:
+        print(f"Ocurrió un error inesperado: {e}")
+
+def generar_archivos():
+    """Genera un archivo .txt con todos los datos"""
+    try:
+        #inicializamos la variable vacia
+        ventas_total = 0
+
+        #Abrimos el archivo para leer los datos
+        lector = lectura()
+
+        for fila in lector:
+            #Como usamos DictReader, accedemos al nombre de la columna directamente
             precio_total = float(fila["Total Amount"])
 
-            """Calculamos las ventas totales"""
+            #Calculamos las ventas totales
             ventas_total += precio_total
 
-        """Llamamos a las funciones para obtener los datos ya calculados y no tener que repetir código"""
+        #Llamamos a las funciones para obtener los datos ya calculados y no tener que repetir código
         total_por_mes = ventas_por_mes()
         ventas_producto = producto_mas_vendido()
          
         mayor_vendido = max(ventas_producto, key=ventas_producto.get)
 
-        """Guardamos los resultados obtenitdos en un archivo .txt"""
+        #Guardamos los resultados obtenidos en un archivo .txt
         with open (ARCHIVO_SALIDA, "w", encoding="utf-8") as arch_out:
             arch_out.write("=== REPORTE ===\n")
             arch_out.write(f"VENTAS TOTALES:\n ${ventas_total:,.2f}\n")
@@ -144,7 +174,11 @@ def generar_archivo():
             for mes, total in sorted(total_por_mes.items()):
                 arch_out.write(f"{mes}, se vendió ${total:,.2f}\n")
 
-        print("BIEN! El archivo se generó exitosamente")
+        #Llamamos a las funciones para generar el gŕafico y el archivo .csv
+        generar_grafico_ventas_por_mes(total_por_mes)
+        generar_tabla_por_categoria()
+        
+        print("BIEN! El archivo y los resultados se generaron exitosamente")
 
 
     except FileNotFoundError:
@@ -156,34 +190,49 @@ def generar_archivo():
     except Exception as e:
         print(f"Ocurrió un error inesperado: {e}")
 
-
-
-
-# Menú principal
-print("-"*30)
-print("    CONSULTAS DE VENTAS")
-print("-"*30)
-print("MENU")
-print(" 1- Búsqueda\n 2- Ventas Totales (por categoría)\n 3- Producto más vendido\n 4- Ventas mensuales\n 5- Generar archivo .txt\n 6- Salir")
-
-while True:
-    opcion = input('\nIngrese una opción del menú: ')
+def generar_grafico_ventas_por_mes(venta_mes):
+    """Genera un gráfico tomando los valores totales de ventas de cada mes"""
     
-    match opcion:
-        case "1":
-            buscador()
-        case "2":
-            ventas_totales_por_categoria()
-        case "3":
-            ventas_producto = producto_mas_vendido()
-            mayor_vendido = max(ventas_producto, key=ventas_producto.get)
-            print(f"\nEl producto más vendido es {mayor_vendido}, con {ventas_producto[mayor_vendido]} unidades vendidas.")
-        case "4":
-            imprimir_ventas_por_mes(ventas_por_mes())
-        case "5":
-            generar_archivo()
-        case "6":
-            print('\nGracias por visitarnos!! Nos vemos la proxima\n')
-            break
-        case _:
-            print("Opción inválida. Intente nuevamente")
+    try:
+        meses = list(venta_mes.keys())
+        totales = list(venta_mes.values())
+        
+        plt.figure(figsize=(10,5))
+        plt.plot(meses, totales, marker="o", color="blue")
+        plt.title("Evolución de Ventas por Mes")
+        plt.xlabel("Mes")
+        plt.ylabel("Ventas ($)")
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        plt.savefig(os.path.join(CARPETA_SALIDA, "ventas_por_mes.png"))
+        plt.close()
+        
+        print("Gráfico generado en resultados/ventas_por_mes.png")
+
+    except Exception as e:
+        print(f"Error al generar el gráfico: {e}")
+
+def generar_tabla_por_categoria():
+    """Genera un archivo .csv con los valores de ventas de cada producto"""
+    
+    try:
+        lector = lectura()
+        
+        if not lector:
+            return
+        
+        df = pd.DataFrame(lector)
+        df["Total Amount"] = df["Total Amount"].astype(float)
+        tabla = df.groupby("Product Category")["Total Amount"].sum()
+        tabla.to_csv(os.path.join(CARPETA_SALIDA, "tabla_ventas.csv"))
+        
+        print("Tabla generada en resultados/tabla_ventas.csv")
+    
+    except KeyError as e:
+        print(f"Error: Falta la columna {e}.")
+
+    except ValueError as e:
+        print(f"Error en los datos: {e}")
+
+    except Exception as e:
+        print(f"Error al generar la tabla: {e}")
